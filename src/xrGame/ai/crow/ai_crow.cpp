ebedc7d7a079587c5edfc1581d093e8c297414d7
ShokerStlk <shoker20008@rambler.ru>
2017-03-01 02:56:57 +0300

    Various fixes to crow's class by Sin!

--- src/xrGame/ai/crow/ai_crow.cpp
+++ src/xrGame/ai/crow/ai_crow.cpp
@@ -143,8 +143,26 @@ BOOL CAI_Crow::net_Spawn(CSE_Abstract* DC)
     m_Anims.m_fly.Load(M, "fly_fwd");
     m_Anims.m_idle.Load(M, "fly_idle");
 
-    // disable UpdateCL, enable only on HIT
-    processing_deactivate();
+    o_workload_frame = 0;
+    o_workload_rframe = 0;
+
+    if (GetfHealth() > 0) {
+        st_current = ECrowStates::eFlyIdle;
+        st_target = ECrowStates::eFlyIdle;
+
+        // disable UpdateCL, enable only on HIT
+        processing_deactivate();
+    }
+    else
+    {
+        st_current = ECrowStates::eDeathFall;
+        st_target = ECrowStates::eDeathDead;
+
+        // turn on physic
+        processing_activate();
+        CreateSkeleton();
+    }
+
     VERIFY2(valid_pos(Position()), dbg_valide_pos_string(Position(), this, "CAI_Crow::net_Spawn"));
     return R;
 }
@@ -291,7 +309,7 @@ void CAI_Crow::UpdateCL()
 }
 void CAI_Crow::renderable_Render()
 {
-    UpdateWorkload(Device.fTimeDelta);
+    UpdateWorkload(Device.fTimeDelta * (Device.dwFrame - o_workload_frame));
     inherited::renderable_Render();
     o_workload_rframe = Device.dwFrame;
 }
@@ -351,7 +369,7 @@ void CAI_Crow::shedule_Update(u32 DT)
     m_Sounds.m_idle.SetPosition(Position());
 
     // work
-    if (o_workload_rframe == (Device.dwFrame - 1))
+    if (o_workload_rframe >= (Device.dwFrame - 2))
         ;
     else
         UpdateWorkload(fDT);
